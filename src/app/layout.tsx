68a51cc58ca99fea5510ff01d6ae0756228a2bd1
notyourbasiclocksmith <notyourbasiclocksmith@users.noreply.github.com>
2026-03-05 19:13:22 -0600

    Fix favicon 404 errors, remove invalid reCAPTCHA, add netlify.toml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -122,6 +122,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
